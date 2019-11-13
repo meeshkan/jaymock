7af10c0d22af9897f9f6a0b86ce97dd612783ca7
k4m4 <nikolaskam@gmail.com>
2019-11-13 15:05:28 -0500

    Create test fixtures

--- test.js
+++ test.js
@@ -1,2 +1,58 @@
 import test from 'ava'
 const jaymock = require('./index')
+
+const fixtures = {
+    flat: {
+        firstName: 'name.firstName',
+        lastName: 'name.lastName'
+    },
+    nested: {
+        addresses: {
+            homeAddress: {
+                streetAddress: 'address.streetAddress',
+                city: 'address.city',
+                zipCode: 'address.zipCode'
+            },
+            workAddress: {
+                streetAddress: 'address.streetAddress',
+                city: 'address.city',
+                zipCode: 'address.zipCode'            
+            }
+        }
+    },
+    repeat: {
+        firstName: 'name.firstName',
+        lastName: 'name.lastName',
+        _repeat: 5
+    },
+    repeatNested: {
+        firstName: 'name.firstName',
+        lastName: 'name.lastName',
+        ipAddress: {
+            ipv4: 'internet.ip',
+            ipv6: 'internet.ipv6',
+            _repeat: 3
+        }
+    },
+    arrayFunction: {
+        ipAddress: 'internet.ip|5'
+    },
+    customFunction: {
+        color: 'hexColor'
+    },
+    customArrayFunction: {
+        color: 'hexColor|10'
+    },
+    fakerFake: {
+        fullName: 'fake({{name.lastName}}, {{name.firstName}} {{name.suffix}})'
+    },
+    invalidFakerFunction: {
+        invalid: 'name.doesnt_exist'
+    },
+    fakerLocale: {
+        name: 'name.firstName'
+    },
+    fakerSeed: {
+        number: 'random.number'
+    }
+}
