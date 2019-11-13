294137ab6649fdd93be638e61af343af5ddac7c0
k4m4 <nikolaskam@gmail.com>
2019-11-13 14:55:04 -0500

    Create helper functions

--- index.js
+++ index.js
@@ -6,3 +6,12 @@
 
 const faker = require('faker')
 const cloneDeep = require('lodash.clonedeep')
+
+/**
+ * Helper functions.
+ */
+
+const isType = (value, type) => typeof value === type
+const isObject = value => isType(value, 'object')
+const generateArrayOfLength = length => [...Array(length)]
+const isObjectKey = (key, object) => Object.keys(object).includes(key)
