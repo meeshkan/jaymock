dc2b2913b64e090716740da0c7bd75ff8637d8f3
k4m4 <nikolaskam@gmail.com>
2019-12-07 21:56:31 -0500

    Create TypeScript type definition file

---
+++ index.d.ts
@@ -0,0 +1,45 @@
+declare namespace jaymock {
+    interface Template {
+        [key: string]: string | number | Template;
+    }
+    interface Populated {
+        [key: string]: unknown | Array<Populated> | Populated;
+    }
+    type ExtensionName = string | {[key: string]: Function};
+    type Functions = {[key: string]: Function};
+}
+
+declare const jaymock: {
+    /**
+    Instantiates an empty `JayMock` object.
+    */
+	(): {template: jaymock.Template, functions: jaymock.Functions};
+
+	/**
+    Populates the template object with fake data.
+    @param template - A [`template`](https://github.com/unmock/jaymock#template) object to populate.
+	@returns An `object` populated with fake data.
+    */
+    populate(template: jaymock.Template): jaymock.Populated;
+    
+    /**
+    Adds a custom data generation function that can be called in the `populate` `template` object using the value of its `name`.
+    @param name - A `string` used to refer to the `body` function.
+    @param body - A `function` that can be referred to in the `template` object.
+    */
+    extend(name: jaymock.ExtensionName, body?: Function): void;
+    
+    /**
+    Sets `Faker.js`'s language locale.
+    @param locale - `Faker.js` locale.
+    */
+    setFakerLocale(locale: string): void;
+    
+    /**
+    Sets `Faker.js`'s randomness seed.
+    @param seed - `Faker.js` randomness seed.
+    */
+    setFakerSeed(seed: number): void;
+};
+
+export = jaymock;
