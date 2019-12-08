2e179ab06c8581c5719d595bad90b4a7b661a1bc
k4m4 <nikolaskam@gmail.com>
2019-12-07 22:27:37 -0500

    Fix TypeScript types

--- index.d.ts
+++ index.d.ts
@@ -1,45 +1,42 @@
 declare namespace jaymock {
+    type ExtensionName = string | {[key: string]: Function};
+    type Functions = {[key: string]: Function};
+    type Instance = {template: jaymock.Template, functions: jaymock.Functions}
     interface Template {
         [key: string]: string | number | Template;
     }
     interface Populated {
         [key: string]: unknown | Array<Populated> | Populated;
     }
-    type ExtensionName = string | {[key: string]: Function};
-    type Functions = {[key: string]: Function};
+    interface Method {
+        /**
+        Populates the template object with fake data.
+        @param template - A [`template`](https://github.com/unmock/jaymock#template) object to populate.
+        @returns An `object` populated with fake data.
+        */
+        populate(template: jaymock.Template): jaymock.Populated;
+
+        /**
+        Adds a custom data generation function that can be called in the `populate` `template` object using the value of its `name`.
+        @param name - A `string` used to refer to the `body` function.
+        @param body - A `function` that can be referred to in the `template` object.
+        */
+        extend(name: jaymock.ExtensionName, body?: Function): void;
+
+        /**
+        Sets `Faker.js`'s language locale.
+        @param locale - `Faker.js` locale.
+        */
+        setFakerLocale(locale: string): void;
+
+        /**
+        Sets `Faker.js`'s randomness seed.
+        @param seed - `Faker.js` randomness seed.
+        */
+        setFakerSeed(seed: number): void;
+    }
 }
 
-declare const jaymock: {
-    /**
-    Instantiates an empty `JayMock` object.
-    */
-	(): {template: jaymock.Template, functions: jaymock.Functions};
-
-	/**
-    Populates the template object with fake data.
-    @param template - A [`template`](https://github.com/unmock/jaymock#template) object to populate.
-	@returns An `object` populated with fake data.
-    */
-    populate(template: jaymock.Template): jaymock.Populated;
-    
-    /**
-    Adds a custom data generation function that can be called in the `populate` `template` object using the value of its `name`.
-    @param name - A `string` used to refer to the `body` function.
-    @param body - A `function` that can be referred to in the `template` object.
-    */
-    extend(name: jaymock.ExtensionName, body?: Function): void;
-    
-    /**
-    Sets `Faker.js`'s language locale.
-    @param locale - `Faker.js` locale.
-    */
-    setFakerLocale(locale: string): void;
-    
-    /**
-    Sets `Faker.js`'s randomness seed.
-    @param seed - `Faker.js` randomness seed.
-    */
-    setFakerSeed(seed: number): void;
-};
+declare function jaymock(): jaymock.Instance & jaymock.Method;
 
 export = jaymock;
